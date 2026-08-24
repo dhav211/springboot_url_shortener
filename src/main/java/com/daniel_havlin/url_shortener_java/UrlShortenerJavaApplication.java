bbdea49a01af0d5f420066c3a77bec4a4a60c2c0
dhav211 <dhav211@gmail.com>
2026-08-24 16:11:52 -0700

    first commit

---
+++ src/main/java/com/daniel_havlin/url_shortener_java/UrlShortenerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.daniel_havlin.url_shortener_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel_havlin/url_shortener_java/UrlShortenerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel_havlin.url_shortener_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
